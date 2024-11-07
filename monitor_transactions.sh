3d0eead4a19f398907d6bdc159c7a0e2672377ee
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2024-11-07 09:23:58 +0530

    Update monitor_transactions.sh

--- monitor_transactions.sh
+++ monitor_transactions.sh
@@ -1,1 +1,8 @@
+#Create a monitor_transactions.sh script that: * Scans the transaction_log.txt. * Identifies any withdrawals of ₹50,000 or more and prints a summary of those transactions.
+
+#Sample output:
+
+#Suspicious Transactions:  2024-10-26 14:10:32 | Account: 67890 | Withdraw: 50000
+
+
 #todo
