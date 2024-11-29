83550667ff0f88a697476866fb4c62f2e3e295c1
thiru170398 <thiru3698@gmail.com>
2024-11-29 23:29:43 +0530

    Update transaction.sh

--- transaction.sh
+++ transaction.sh
@@ -6,10 +6,13 @@ transaction_type=$2
 amount=$3
 
 # Get the current timestamp
-timestamp= #todo
+timestamp=$(date +'%Y-%m-%d %H:%M:%S')
 
 # Log the transaction in transaction_log.txt
-#todo
+echo $timestamp "|" "Account: "$1 "|" $2":" $3 >> transaction_log.txt
+if (("$2"=="withdraw" && $3>=50000)); then
+echo "ALERT: High-value transaction detected!"
+fi
 
 # Check if the transaction is a high-value withdrawal
 #todo
