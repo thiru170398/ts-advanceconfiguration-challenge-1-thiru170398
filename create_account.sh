a313289128bd5d457aed6c8721e6f9fd1962d8ac
thiru170398 <thiru3698@gmail.com>
2024-11-29 23:23:11 +0530

    Update create_account.sh

--- create_account.sh
+++ create_account.sh
@@ -5,13 +5,14 @@ username=$1
 account_number=$2
 
 # Default balance for new accounts
-default_balance= #todo
-
+default_balance= 1000
 # Check if account already exists in accounts.txt
-#todo
-
+if grep -q "Account:$2" accounts.txt; then 
+echo "Account already Exists"
+else
 # Add the new account to accounts.txt
-#todo
+echo "username: "$1 "|" "Account:" $2 "|" "Balance:" $default_balance >> accounts.txt
+fi
 
 # Create the user in the system (if running in a real Linux environment)
 #todo
