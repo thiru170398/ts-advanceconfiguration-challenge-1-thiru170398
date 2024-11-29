91eb67ebfe2447135128a2e8b29e9244ddecdbaf
thiru170398 <thiru3698@gmail.com>
2024-11-29 23:17:21 +0530

    Update monitor_transactions.sh

--- monitor_transactions.sh
+++ monitor_transactions.sh
@@ -3,3 +3,14 @@
 
 #Sample output: 
   # Suspicious Transactions:  2024-10-26 14:10:32 | Account: 67890 | Withdraw: 50000
+while IFS="|" read col1 col2 col3
+do
+echo $col3>>out.txt
+done < transaction_log.txt
+
+while IFS=":" read c1 c2
+do
+if (("$c1"=="withdrawals" && $c2>=50000)); then
+echo $c1":" $c2
+fi
+done < out.txt
